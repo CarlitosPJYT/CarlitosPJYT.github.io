111d64237c28b42f8424605d45ea3764486d2a84
CarlitosJPYT <59419806+CarlitosJPYT@users.noreply.github.com>
2020-07-16 21:03:27 -0500

    Both

---
+++ script.js
@@ -0,0 +1,21 @@
+// Click the button to change the color of this paragraph<br><br>function myFunction() {<br>
+var x;
+x = document.getElementById("demo");
+x.style.fontSize = "25px";
+x.style.color = "red";
+
+function w3_open() {
+     document.getElementById("mySidebar").style.display = "block";
+     document.getElementById("myOverlay").style.display = "block";
+}    
+function w3_close() {
+     document.getElementById("mySidebar").style.display = "none";
+     document.getElementById("myOverlay").style.display = "none";
+}   
+function w3_show_nav(name) {
+     document.getElementById("menuTut").style.display = "none";
+     document.getElementById("menuRef").style.display = "none";
+     document.getElementById(name).style.display = "block";
+     w3-open();
+}
+w3CodeColor();
